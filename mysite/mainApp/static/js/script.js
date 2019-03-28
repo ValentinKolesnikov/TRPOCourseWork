13dbbfbc7223a519daa4025809098269907623e9
ValentinKolesnikov <valentin.kolesnikov.work@gmail.com>
2019-03-28 11:20:59 +0300

    Valik update autoriz

---
+++ mysite/mainApp/static/js/script.js
@@ -0,0 +1,8 @@
+if(navigator.userAgent.toLowerCase().indexOf("chrome") >= 0 || navigator.userAgent.toLowerCase().indexOf("safari") >= 0){
+window.setInterval(function(){
+    $('input:-webkit-autofill').each(function(){
+        var clone = $(this).clone(true, true);
+        $(this).after(clone).remove();
+    });
+}, 20);
+}
