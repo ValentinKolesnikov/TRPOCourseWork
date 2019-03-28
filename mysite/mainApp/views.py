2055927a3a31352b1431f31c41ea1ec390bf1d7b
oleg <oleg.fedoseev.00@mail.ru>
2019-03-28 13:58:43 +0300

    oleg update 0.6

--- mysite/mainApp/views.py
+++ mysite/mainApp/views.py
@@ -1,11 +1,16 @@
 from catalog.models import Restaurant
+from loginsystem.forms import RegisterForm
 from django.shortcuts import render
 from django.contrib import auth
 
 def index(request):
+    args = {}
+    args['form'] = RegisterForm()
     object_list = Restaurant.objects.all()
     count = object_list.count
-    return render(request,'mainApp/homePage.html',{'count':count})
+    args['count'] = count
+
+    return render(request,'mainApp/homePage.html',args)
 
 def contact(request):
     return render(request,'mainApp/basic.html', {'values':['Вопросы - звоните','375296497315']})
