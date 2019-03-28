179bb147b56466437bdd93a046ed49272493b937
ValentinKolesnikov <valentin.kolesnikov.work@gmail.com>
2019-03-28 12:41:19 +0300

    Valik update

--- mysite/mainApp/static/js/script.js
+++
@@ -1,8 +0,0 @@
-if(navigator.userAgent.toLowerCase().indexOf("chrome") >= 0 || navigator.userAgent.toLowerCase().indexOf("safari") >= 0){
-window.setInterval(function(){
-    $('input:-webkit-autofill').each(function(){
-        var clone = $(this).clone(true, true);
-        $(this).after(clone).remove();
-    });
-}, 20);
-}
